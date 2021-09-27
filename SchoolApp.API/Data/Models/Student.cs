80b4b9cf6506e0442c1cd6ecff45e2728ec74235
muhammetandic <muhammet.andic@gmail.com>
2021-09-27 20:41:26 +0000

    Name prop added to Student class

--- SchoolApp.API/Data/Models/Student.cs
+++ SchoolApp.API/Data/Models/Student.cs
@@ -5,6 +5,7 @@ namespace JwtAuth.API.Data.Models
     public class Student
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string FullName { get; set; }
         public DateTime DateOfBirth { get; set; }
     }
